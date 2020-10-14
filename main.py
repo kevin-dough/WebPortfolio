c005b6b8bd3a04e21438f6fded70762d0da7fd45
Kevin Do <kevin12345do@gmail.com>
2020-10-14 23:30:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# https://flask.palletsprojects.com/en/1.1.x/api/
+from flask import Flask, render_template
+#create a Flask instance
+app = Flask(__name__)
+
+#connects default URL of server to a python function
+@app.route('/')
+def home():
+    #function use Flask import (Jinga) to render an HTML template
+    return render_template("home.html")
+
+@app.route('/kevin')
+def kevin():
+    return render_template("kevin.html")
+
+@app.route('/abhijay')
+def abhijay():
+    return render_template("abhijay.html")
+
+if __name__ == "__main__":
+    #runs the application on the repl development server
+    app.run(debug=True, port='3000', host='0.0.0.0')
